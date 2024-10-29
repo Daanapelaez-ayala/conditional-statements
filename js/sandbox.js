93b935328fa95edea7250f6e082dce0263a803de
daana <126dpelaezayala@frhsd.com>
2024-10-29 13:15:28 -0400

    Message

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,12 +1,24 @@
 const d= new Date ();
 let hour=d.getHours();
 
+console.log(hour);
+
 if(hour<12){
     console.log("morning");
+    document.getElementById("greetings").innerText = "Goodmorning";
+    document.getElementById("picture").src = "img/sun.png";
 }
+
 else if  (hour<17){
     console.log("afternoon");
+    document.getElementById("greetings").innerText = "GoodAfternoon";
+    document.getElementById("picture").src = "img/cloud.png";
 }
+
 else {
     console.log("night");
+    document.getElementById("greetings").innerText = "Goodnight";
+    document.getElementById("picture").src = "img/night.png";
 }
+
+// console.log(greetingElement);
